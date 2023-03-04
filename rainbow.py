dc042066622e09ddd6252e0e98a0e1a96948ed9c
aneeshdurg <aneeshdurg17@gmail.com>
2023-03-04 15:28:00 -0600

    Privatize variables

--- rainbow.py
+++ rainbow.py
@@ -15,9 +15,9 @@ class Rainbow:
     tu: clang.cindex.TranslationUnit
     prefix: str
 
-    fns_to_tags = {}
-    fns_stack = []
-    call_graph = {}
+    _fns_to_tags = {}
+    _call_graph = {}
+    _fns_stack = []
 
     def isFunction(self, node: clang.cindex.Cursor) -> Optional[str]:
         """Determine if `node` is a function definition, and if so, return the name of the function called if possible"""
@@ -56,52 +56,55 @@ class Rainbow:
         # print(" " * indent, node.kind, node.spelling, file=sys.stderr)
         fnname = self.isFunction(node)
         if fnname:
-            if fnname not in self.fns_to_tags:
-                self.fns_to_tags[fnname] = []
-            if fnname not in self.call_graph:
-                self.call_graph[fnname] = []
-            self.fns_stack.append((fnname, depth))
+            if fnname not in self._fns_to_tags:
+                self._fns_to_tags[fnname] = []
+            if fnname not in self._call_graph:
+                self._call_graph[fnname] = []
+            self._fns_stack.append((fnname, depth))
         elif called := self.isCall(node):
-            fn, _ = self.fns_stack[-1]
-            self.call_graph[fn].append(called)
+            fn, _ = self._fns_stack[-1]
+            self._call_graph[fn].append(called)
         elif color := self.isColor(node):
-            fn, fdepth = self.fns_stack[-1]
+            fn, fdepth = self._fns_stack[-1]
             if fdepth == (depth - 1):
-                self.fns_to_tags[fn].append(color)
+                self._fns_to_tags[fn].append(color)
         for c in node.get_children():
             self._process(c, depth + 1)
         if fnname:
-            self.fns_stack.pop()
+            self._fns_stack.pop()
 
     def process(self):
         """Process the input file and extract the call graph, and colors for every function"""
         self._process(self.tu.cursor, 0)
-        assert len(self.fns_stack) == 0
+        assert len(self._fns_stack) == 0
 
     def toCypher(self) -> str:
         """Must only be called after `self.process`.
         Outputs the call graph as an openCypher CREATE query, tagging all functions with their colors"""
-        if len(self.fns_to_tags) == 0 or max(len(t) for t in self.fns_to_tags.values()) == 0:
+        if (
+            len(self._fns_to_tags) == 0
+            or max(len(t) for t in self._fns_to_tags.values()) == 0
+        ):
             return "// no tagged functions\n;\n"
         output = []
         referenced_fns = set()
-        for fn in self.call_graph:
-            if len(self.call_graph[fn]) > 0:
+        for fn in self._call_graph:
+            if len(self._call_graph[fn]) > 0:
                 referenced_fns.add(fn)
-                for callee in self.call_graph[fn]:
+                for callee in self._call_graph[fn]:
                     referenced_fns.add(callee)
 
         output.append("CREATE")
-        for fn in self.fns_to_tags:
+        for fn in self._fns_to_tags:
             if fn not in referenced_fns:
                 continue
-            tags = self.fns_to_tags[fn]
+            tags = self._fns_to_tags[fn]
             tag = ""
             if len(tags):
                 tag = f" :{tags[0]}"
             output.append(f"  ({fn}{tag}),")
-        for caller in self.call_graph:
-            for callee in self.call_graph[caller]:
+        for caller in self._call_graph:
+            for callee in self._call_graph[caller]:
                 output.append(f"  ({caller})-[:CALLs]->({callee}),")
         output.append(" (sentinel)")
         output.append(";")
