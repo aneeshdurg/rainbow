9666a7fc62383a2ed3365c428b885eff965c372e
aneeshdurg <aneeshdurg17@gmail.com>
2023-03-21 19:26:58 -0500

    Track nested scopes correctly

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,12 +1,45 @@
+#include <functional>
 #include <stdio.h>
 
 #define COLOR(x) clang::annotate("COLOR::" #x)
 
+[[COLOR(PURPLE)]] int printf(char *fmt, ...);
+
 [[COLOR(BLUE)]] int ret0() { return 0; };
 [[COLOR(PURPLE)]] int ret_wrapper() { return ret0(); };
+
+typedef void (*VFn)(void);
+
+[[COLOR(RED)]] void unused([[COLOR(BLUE)]] std::function<void()> my_fn,
+                           [[COLOR(BLUE)]] VFn my_fn_2) {
+  my_fn();
+  my_fn_2();
+}
+
+[[COLOR(BLUE)]] void idk() {}
 
 [[COLOR(RED)]] int main() {
   [[COLOR(GREEN)]] int r = 0;
   printf("!!! %d\n", r);
-  return ret_wrapper() + ret0();
+
+  auto BlueFn = []() {};
+  unused(BlueFn, idk);
+
+  {
+    [[COLOR(YELLOW)]] auto WrapperFn1 = []() { return ret0(); };
+    WrapperFn1();
+  }
+
+  if (true) {
+    [[COLOR(PURPLE)]] auto WrapperFn = ({
+      (void)0;
+      []() { return ret0(); };
+    });
+
+    [[COLOR(PURPLE)]] auto WrapperFn1 = []() { return ret0(); };
+    (void)WrapperFn1();
+
+    return ret_wrapper() + ret0(); // + WrapperFn();
+  }
+  return 0;
 }
--- rainbow.py
+++ rainbow.py
@@ -3,13 +3,83 @@ import json
 import subprocess
 import sys
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, MISSING
 from pathlib import Path
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Set, Tuple
 
 import click
 
 import clang.cindex
+
+@dataclass
+class Scope:
+    id_: int
+    functions: Dict[str, 'Function'] = field(default_factory=dict)
+    child_scopes: List['Scope'] = field(default_factory=list)
+    called_functions: List[str] = field(default_factory=list)
+
+    def empty(self):
+        if len(self.functions) != 0:
+            return False
+        if len(self.called_functions) != 0:
+            return False
+
+        if len(self.child_scopes) == 0:
+            return True
+        if all([s.empty() for s in self.child_scopes]):
+            return True
+
+
+    def register_call(self, fnname: str):
+        self.called_functions.append(fnname)
+
+    def dump(self, level: int = 0):
+        prefix = " " * (2 * level)
+        print("{", self.id_)
+        print(prefix, " ", "Functions")
+        for f in self.functions:
+            fn = self.functions[f]
+            if fn.get_scope().empty() and fn.color is None:
+                continue
+            print(prefix, "  ", f, end=" ")
+            fn.dump(level + 2)
+        print(prefix, " ", "Called_functions")
+        for f in self.called_functions:
+            print(prefix, "   ", f)
+        print(prefix, " ", "Child Scopes")
+        for i, f in enumerate(self.child_scopes):
+            if not f.empty():
+                print(prefix, "  ", i, ":", end=" ")
+                f.dump(level + 2)
+        print(prefix + "}")
+
+
+@dataclass
+class Function:
+    name: str
+    color: Optional[str]
+    params: Dict[str, str]
+    parent_scope: Scope
+    inner_scope_id: int
+    inner_scope_: Scope = Scope(-1)
+
+    def __post_init__(self):
+        self.inner_scope_ = Scope(self.inner_scope_id)
+
+    def alias(self) -> str:
+        return f"{self.name}_{self.inner_scope_id}"
+
+    def get_scope(self) -> Scope:
+        return self.inner_scope_
+
+    def dump(self, level: int):
+        print("{")
+        prefix = " " * (2 * level)
+        print(prefix, " Color:", self.color)
+        print(prefix, " Params:", self.params)
+        print(prefix, " Scope:", end=" ")
+        self.inner_scope_.dump(level + 1)
+        print(prefix + "}")
 
 
 @dataclass
@@ -20,16 +90,37 @@ class Rainbow:
 
     _fns_to_tags: Dict[str, List[str]] = field(default_factory=dict)
     _call_graph: Dict[str, List[str]] = field(default_factory=dict)
-    _fns_stack: List[Tuple[str, int]] = field(default_factory=list)
+
+    _global_scope: Scope = Scope(0)
+    _scope_id_vendor: int = 0
+
+    # Set of unsupported types we've already emitted warnings for
+    _seen_unsupported_types: Set[clang.cindex.CursorKind] = field(default_factory=set)
+
+    def _get_new_scope_id(self) -> int:
+        self._scope_id_vendor += 1
+        return self._scope_id_vendor
+
+    def isLambda(self, node: clang.cindex.Cursor)-> bool:
+        return node.kind == clang.cindex.CursorKind.LAMBDA_EXPR
+
+    def isScope(self, node: clang.cindex.Cursor) -> bool:
+        return node.kind == clang.cindex.CursorKind.COMPOUND_STMT
+
+    def isVarDecl(self, node: clang.cindex.Cursor) -> bool:
+        return node.kind == clang.cindex.CursorKind.VAR_DECL
 
     def isFunction(self, node: clang.cindex.Cursor) -> Optional[str]:
         """Determine if `node` is a function definition, and if so, return the name of the function called if possible"""
-        if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+        if node.kind in [clang.cindex.CursorKind.FUNCTION_DECL, clang.cindex.CursorKind.FUNCTION_TEMPLATE]:
             return node.spelling
         for c in node.get_children():
-            if c.kind == clang.cindex.CursorKind.LAMBDA_EXPR:
-                raise Exception("Lambdas unsupported")
-                # return "{node.spelling}[[{c.spelling}]]"
+            if self.isLambda(c):
+                parent = c.semantic_parent
+                if self.isVarDecl(parent):
+                    print("FOUND LAMBDA NAMED", parent.spelling)
+                    return parent.spelling
+                raise Exception("Unnamed lambda unsupported")
         return None
 
     def isCall(self, node: clang.cindex.Cursor) -> Optional[str]:
@@ -54,45 +145,106 @@ class Rainbow:
                 return node.spelling[len(self.prefix) :]
         return None
 
-    def _register_function(self, fnname: str, depth: int):
-        if fnname not in self._fns_to_tags:
-            self._fns_to_tags[fnname] = []
-        if fnname not in self._call_graph:
-            self._call_graph[fnname] = []
-        self._fns_stack.append((fnname, depth))
+    def _process_function(self, fnname: str, node: clang.cindex.Cursor, scope: Scope):
+        # TODO this should also include finding all the callable params
+        # TODO Also need to do a pass verifing that all pass in params have the
+        # right colors.
+        params: Dict[str, str] = {}
+        fn_color: Optional[str] = None
+        body: Optional[clang.cindex.Cursor] = None
+        if self.isLambda(node):
+            parent = node.semantic_parent
+            for c in parent.get_children():
+                if color := self.isColor(c):
+                    if fn_color is not None:
+                        raise Exception(f"Multiple colors found for function {fnname}")
+                    fn_color = color
 
-    def _register_call(self, called_fn: str):
-        fn, _ = self._fns_stack[-1]
-        # TODO(aneesh) can lambdas shadow names of functions? This might need to also use the depth if so
-        self._call_graph[fn].append(called_fn)
-
-    def _try_tag_function(self, color: str, depth: int):
-        fn, fdepth = self._fns_stack[-1]
-        if fdepth == (depth - 1):
-            if color not in self.colors:
-                raise Exception(f"Could not tag function {fn} with unknown color {color}")
-            self._fns_to_tags[fn].append(color)
-
-    def _process(self, node: clang.cindex.Cursor, depth: int):
-        indent = depth * 2
-        # print(" " * indent, node.kind, node.spelling, file=sys.stderr)
-        fnname = self.isFunction(node)
-        if fnname:
-            self._register_function(fnname, depth)
-        elif called := self.isCall(node):
-            self._register_call(called)
-        elif color := self.isColor(node):
-            self._try_tag_function(color, depth)
         for c in node.get_children():
-            self._process(c, depth + 1)
+            if color := self.isColor(c):
+                if fn_color is not None:
+                    raise Exception(f"Multiple colors found for function {fnname}")
+                fn_color = color
+            elif c.kind == clang.cindex.CursorKind.PARM_DECL:
+                param_name = c.spelling
+                param_color = None
+                for param_child in c.get_children():
+                    if color := self.isColor(param_child):
+                        if param_color is not None:
+                            raise Exception(f"Multiple colors found for param {param_name} of function {fnname}")
+                        param_color = color
+                if param_color:
+                    params[param_name] = param_color
+            elif self.isScope(c):
+                if body is not None:
+                    raise Exception("?")
+                body = c
 
-        if fnname:
-            self._fns_stack.pop()
+        if fnname in scope.functions:
+            fn = scope.functions[fnname]
+            if fn_color and fn.color:
+                if fn.color != fn_color:
+                    raise Exception(f"Multiple colors found for function {fnname}")
+            else:
+                fn.color = fn_color
+
+            for param_name in params:
+                if param_name in fn.params:
+                    if fn.params[param_name] != params[param_name]:
+                        raise Exception(f"Multiple colors found for param {param_name} of function {fnname}")
+                else:
+                    fn.params[param_name] = params[param_name]
+        else:
+            scope_id = self._get_new_scope_id()
+            fn = Function(fnname, fn_color, params, scope, scope_id)
+            scope.functions[fnname] = fn
+
+        # This might just be a declaration, so there might not be a function
+        # body
+        if body:
+            self._process(body, fn.get_scope(), None);
+
+    def isUnsupported(self, node: clang.cindex.Cursor):
+        unsupported_types = [
+            clang.cindex.CursorKind.CXX_METHOD,
+            clang.cindex.CursorKind.StmtExpr,
+        ]
+        return node.kind in unsupported_types
+
+    def _process(self, node: clang.cindex.Cursor, scope: Scope, decl_color: Optional[str]):
+        if self.isUnsupported(node):
+            if node.kind not in self._seen_unsupported_types:
+                self._seen_unsupported_types.add(node.kind)
+                print("Warning unsupported node type", node.kind, file=sys.stderr)
+            return
+
+        # TODO(aneesh) Support namespaces and namespaced functions
+
+        if self.isScope(node):
+            scope_id = self._get_new_scope_id()
+            new_scope = Scope(scope_id)
+            scope.child_scopes.append(new_scope)
+            for c in node.get_children():
+                self._process(c, new_scope, decl_color)
+            return
+
+        if fnname := self.isFunction(node):
+            self._process_function(fnname, node, scope)
+            return
+
+        if called := self.isCall(node):
+            scope.register_call(called)
+            # TODO can lambdas shadow names of functions? This might need to also use the depth if so
+            # TODO need to implement some kind of scope for the above
+            #  self._call_graph[fn].append(called_fn)
+            return
+
+        for c in node.get_children():
+            self._process(c, scope, decl_color)
 
     def process(self):
         """Process the input file and extract the call graph, and colors for every function"""
-        self._process(self.tu.cursor, 0)
-        assert len(self._fns_stack) == 0
+        self._process(self.tu.cursor, self._global_scope, None)
 
     def toCypher(self) -> str:
         """Must only be called after `self.process`.
@@ -168,6 +320,9 @@ def main(cpp_file: str, config_file: str, clanglocation: Optional[Path]):
     # TODO set compilation db if it exists
     rainbow = Rainbow(index.parse(cpp_file), prefix, colors)
     rainbow.process()
+    rainbow._global_scope.dump()
+    return
+
     create_query = rainbow.toCypher()
     validate_queries = patternsToCypher(patterns)
     validate_query = "\n".join(validate_queries)
