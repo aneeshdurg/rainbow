048ba0beecaeec66e73855a756f5d3bcd623e3e4
Aneesh Durg <aneeshdurg17@gmail.com>
2024-08-29 11:50:04 -0500

    Get basic example working

--- llvmpass/rainbow_llvm.py
+++ llvmpass/rainbow_llvm.py
@@ -2,16 +2,23 @@
 Rainbow as an llvm pass
 """
 
-import os
 import logging
+import os
+import subprocess
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 
 import llvmcpy.llvm as cllvm
 from lark import Lark
-import rainbow
 from rainbow.config import Config
 from rainbow.scope import Scope
+
+
+def decode_name(name: str):
+    p = subprocess.Popen(["c++filt"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out = p.communicate(name.encode())
+    return out[0].decode()
 
 
 @dataclass
@@ -27,6 +34,10 @@ def const_str_glbl(m: cllvm.Module, glbl: str) -> str:
     if "COLOR::" in a:
         return a[len("COLOR::") :]
     return a
+
+
+def get_iptr(inst):
+    return str(inst.ptr[0]).split()[-1][:-1]
 
 
 def parse_annotations(annotations: str, m: cllvm.Module) -> dict[str, GlobalFn]:
@@ -98,7 +109,6 @@ def do_rainbow_analysis(m: cllvm.Module):
     annotated_module_fns = parse_annotations(annotations, m)
     for fn in m.iter_functions():
         # Need to check if the fn is linked in
-        # print("gfn", fn.name)
         # TODO get param colors
         fn_name = fn.name.decode()
         # TODO pass in filename/line numbers?
@@ -106,53 +116,53 @@ def do_rainbow_analysis(m: cllvm.Module):
             Scope.create_function(
                 scope_id,
                 root_scope,
-                fn_name,
+                decode_name(fn_name),
                 annotated_module_fns[fn_name].attributes[0],
                 {},
             )
         else:
-            Scope.create_function(scope_id, root_scope, fn_name, None, {})
+            Scope.create_function(scope_id, root_scope, decode_name(fn_name), None, {})
 
     for fn in m.iter_functions():
-        fn_scope = root_scope.resolve_function(fn.name.decode())
+        fn_scope = root_scope.resolve_function(decode_name(fn.name.decode()))
         assert fn_scope
         scope_id += 1
         llvm_fn = m.get_named_function(fn.name.decode())
-        print(fn)
         inst_to_color = {}
+        lambdas = set()
         for bb in llvm_fn.iter_basic_blocks():
             for inst in bb.iter_instructions():
                 opcode = cllvm.Opcode[inst.instruction_opcode]
                 if opcode == "Alloca":
                     parts = inst.print_value_to_string().decode().split()
                     if parts[3].startswith("%class.anon"):
-                        print("  lambda", str(inst.ptr[0]).split()[-1][:-1])
+                        lambdas.add(get_iptr(inst))
                 elif opcode == "Call":
                     called_fn = inst.get_operand(inst.get_num_arg_operands())
                     if called_fn.name.decode().startswith("llvm.var.annotation"):
                         # llvm.var.annotation is a hint to analyzers to annotate a particular
                         # instruction
                         annotated_obj = inst.get_operand(0)
-                        iptr = str(annotated_obj.ptr[0]).split()[-1][:-1]
+                        iptr = get_iptr(annotated_obj)
                         annotation = inst.get_operand(1)
                         color = const_str_glbl(m, annotation.name.decode())
-                        if iptr not in inst_to_color:
-                            inst_to_color[iptr] = []
-                        inst_to_color[iptr].append(color)
-                        print("  coloring ", iptr, color)
+                        assert iptr not in inst_to_color, "duplicate color"
+                        inst_to_color[iptr] = color
                         # TODO set parameter colors here
                     else:
-                        for i in range(inst.get_num_arg_operands()):
-                            iptr = str(inst.get_operand(i).ptr[0]).split()[-1][:-1]
-                        callee_name = called_fn.name.decode()
-                        print("  call", callee_name)
+                        callee_name = decode_name(called_fn.name.decode())
                         callee = root_scope.resolve_function(callee_name)
                         if callee:
+                            # This should be safe because it's a lambda defined
+                            # in this method
+                            if callee_name.endswith("::operator()() const"):
+                                iptr = get_iptr(inst.get_operand(0))
+                                if iptr in lambdas and iptr in inst_to_color:
+                                    callee.color = inst_to_color[iptr]
                             fn_scope.register_call_scope(callee)
 
-    print()
-    print(root_scope.to_cypher())
-    print(config.run(root_scope))
+    if config.run(root_scope):
+        sys.exit(1)
 
 
 def run_on_module(m: cllvm.Module) -> int:
